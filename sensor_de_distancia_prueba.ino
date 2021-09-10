2e8b6c140ce70efe458f43243d66ef46b0ceb454
Tuplaxd <90460671+Tuplaxd@users.noreply.github.com>
2021-09-10 15:11:23 -0400

    Add files via upload

---
+++ sensor_de_distancia_prueba.ino
@@ -0,0 +1,45 @@
+const int pinecho = 8;
+const int pintrigger = 9;
+const int pinled = 3;
+ 
+// VARIABLES PARA CALCULOS
+unsigned int tiempo, distancia;
+ 
+void setup() {
+  // PREPARAR LA COMUNICACION SERIAL
+  Serial.begin(9600);
+  // CONFIGURAR PINES DE ENTRADA Y SALIDA
+  pinMode(pinecho, INPUT);
+  pinMode(pintrigger, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+ 
+void loop() {
+  // ENVIAR PULSO DE DISPARO EN EL PIN "TRIGGER"
+  digitalWrite(pintrigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pintrigger, HIGH);
+  // EL PULSO DURA AL MENOS 10 uS EN ESTADO ALTO
+  delayMicroseconds(10);
+  digitalWrite(pintrigger, LOW);
+ 
+  // MEDIR EL TIEMPO EN ESTADO ALTO DEL PIN "ECHO" EL PULSO ES PROPORCIONAL A LA DISTANCIA MEDIDA
+  tiempo = pulseIn(pinecho, HIGH);
+ 
+  // LA VELOCIDAD DEL SONIDO ES DE 340 M/S O 29 MICROSEGUNDOS POR CENTIMETRO
+  // DIVIDIMOS EL TIEMPO DEL PULSO ENTRE 58, TIEMPO QUE TARDA RECORRER IDA Y VUELTA UN CENTIMETRO LA ONDA SONORA
+  distancia = tiempo / 58;
+ 
+  // ENVIAR EL RESULTADO AL MONITOR SERIAL
+  Serial.print(distancia);
+  Serial.println(" cm");
+  delay(200);
+ 
+  // ENCENDER EL LED CUANDO SE CUMPLA CON CIERTA DISTANCIA
+  if (distancia <= 15) {
+    digitalWrite(3, HIGH);
+    delay(500);
+  } else {
+    digitalWrite(3, LOW);
+  }
+}
